543a29f88425ea87ce99d9b81885a59497409f48
juancoquet <juancoquet@gmail.com>
2025-11-15 13:49:20 +0000

    feat: add device-specific directory utilities and improve code formatting
    Signed-off-by: juancoquet <juancoquet@gmail.com>

--- highlightscreensaver.koplugin/main.lua
+++ highlightscreensaver.koplugin/main.lua
@@ -11,51 +11,68 @@ local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local Screensaver = require("ui/screensaver")
 local _ = require("gettext")
+local Device = require("device")
+local lfs = require("libs/libkoreader-lfs")
 
 local HighlightScreensaver = WidgetContainer:extend({
-    name = "Highlight Screensaver",
-    is_doc_only = false,
+	name = "Highlight Screensaver",
+	is_doc_only = false,
 })
 
 function HighlightScreensaver:init()
-    self.ui.menu:registerToMainMenu(self)
+	self.ui.menu:registerToMainMenu(self)
 end
 
 function HighlightScreensaver:addToMainMenu(menu_items)
-    menu_items.highlight_screensaver = {
-        text = _("Highlight Screensaver"),
-        -- in which menu this should be appended
-        sorting_hint = "tools",
-        sub_item_table = {
-            {
-                text = _("Scan all book highlights"),
-                -- a callback when tapping
-                callback = function()
-                    self:scanHighlights()
-                end,
-            },
-            {
-                text = _("Add to scannable directories"),
-                callback = function()
-                    self:addToScannableDirectories()
-                end,
-            },
-        },
-    }
+	menu_items.highlight_screensaver = {
+		text = _("Highlight Screensaver"),
+		-- in which menu this should be appended
+		sorting_hint = "tools",
+		sub_item_table = {
+			{
+				text = _("Scan all book highlights"),
+				-- a callback when tapping
+				callback = function()
+					self:scanHighlights()
+				end,
+			},
+			{
+				text = _("Add to scannable directories"),
+				callback = function()
+					self:addToScannableDirectories()
+				end,
+			},
+		},
+	}
 end
 
 function HighlightScreensaver:scanHighlights()
-    local popup = InfoMessage:new({
-        text = _("Highlight Screensaver popup"),
-    })
-    UIManager:show(popup)
+	local popup = InfoMessage:new({
+		text = _("Highlight Screensaver popup"),
+	})
+	UIManager:show(popup)
+end
+
+local function getDefaultRootDir()
+	if Device:isCervantes() or Device:isKobo() then
+		return "/mnt"
+	elseif Device:isEmulator() then
+		return lfs.currentdir()
+	else
+		return Device.home_dir or lfs.currentdir()
+	end
+end
+
+local function getPluginDir()
+	return getDefaultRootDir() .. "/onboard/highlight-screensaver"
 end
 
 function HighlightScreensaver:addToScannableDirectories()
-    local popup = InfoMessage:new({
-        text = _("Add to scannable dirs"),
-    })
-    UIManager:show(popup)
+	-- local dirsFile = io.open(getPluginDir() .. "/scannable-dirs.json", "w")
+	local popup = InfoMessage:new({
+		text = _(getPluginDir()),
+	})
+	UIManager:show(popup)
 end
 
 return HighlightScreensaver
