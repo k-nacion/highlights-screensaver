ee5ef76d813b6df029260581d56c881184fe7c35
juancoquet <juancoquet@gmail.com>
2025-11-16 00:25:54 +0000

    refactor: move menu items from main menu to screensaver settings menu
    Signed-off-by: juancoquet <juancoquet@gmail.com>

--- highlightscreensaver.koplugin/main.lua
+++ highlightscreensaver.koplugin/main.lua
@@ -1,6 +1,7 @@
 local json = require("json")
 -- TODO: reimplement dispatcher integration
 local Dispatcher = require("dispatcher") -- luacheck:ignore
+local FileManager = require("apps/filemanager/filemanager")
 local InfoMessage = require("ui/widget/infomessage")
 local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
@@ -14,6 +15,60 @@ local clipper = require("clipper")
 
 local HIGHLIGHTS_MODE = "highlights"
 
+local function scanHighlights()
+	local sidecars = utils.getAllSidecarPaths()
+	local total_saved = 0
+	for _, sidecar in ipairs(sidecars) do
+		local clippings = clipper.extractClippingsFromSidecar(sidecar)
+		for _, clipping in ipairs(clippings) do
+			clipper.saveClipping(clipping)
+			total_saved = total_saved + 1
+		end
+	end
+
+	local popup = InfoMessage:new({
+		text = _("Saved " .. total_saved .. "highlights"),
+	})
+	UIManager:show(popup)
+end
+
+local function addToScannableDirectories()
+	local scannable_dirs = utils.getScannableDirs()
+	local curr_dir = FileManager.instance.file_chooser.path
+	-- local curr_dir = self.ui.file_chooser.path
+	table.insert(scannable_dirs, curr_dir)
+
+	local unique_dirs = {}
+	local seen = {}
+	for _, dir in ipairs(scannable_dirs) do
+		if not seen[dir] then
+			table.insert(unique_dirs, dir)
+			seen[dir] = true
+		end
+	end
+
+	local dir = utils.getPluginDir()
+	local attr = lfs.attributes(dir)
+	if not attr then
+		local ok, err = utils.makeDir(utils.getPluginDir())
+		if not ok then
+			error(tostring(err))
+		end
+	end
+
+	local file, err = io.open(utils.getScannableDirsFilePath(), "w")
+	if not file then
+		error("Failed to open scannable-dirs file: " .. tostring(err))
+	end
+	file:write(json.encode(unique_dirs))
+	file:close()
+
+	local popup = InfoMessage:new({
+		text = _("Added to scannable directories: " .. curr_dir),
+	})
+	UIManager:show(popup)
+end
+
 -- patch `dofile` to add a highlights mode
 local orig_dofile = dofile
 _G.dofile = function(filepath)
@@ -21,11 +76,28 @@ _G.dofile = function(filepath)
 
 	if filepath and filepath:match("screensaver_menu%.lua$") then
 		if result and result[1] and result[1].sub_item_table then
-      local sleep_screen_menu = result[1]
-			local wallpaper_submenu = sleep_screen_menu.sub_item_table
+			local wallpaper_submenu = result[1].sub_item_table
+
+			table.insert(result, 3, {
+				text = _("Highlights screensaver"),
+				sub_item_table = {
+					{
+						text = _("Scan all book highlights"),
+						callback = function()
+							scanHighlights()
+						end,
+					},
+					{
+						text = _("Add to scannable directories"),
+						callback = function()
+							addToScannableDirectories()
+						end,
+					},
+				},
+			})
 
 			table.insert(wallpaper_submenu, 6, {
-				text = _("Show random highlight on sleep screen"),
+				text = _("Show highlights screensaver"),
 				checked_func = function()
 					return G_reader_settings:readSetting("screensaver_type") == HIGHLIGHTS_MODE
 				end,
@@ -68,88 +140,10 @@ Screensaver.show = function(self)
 end
 
 local HighlightScreensaver = WidgetContainer:extend({
-	name = "Highlight Screensaver",
+	name = "Highlights Screensaver",
 	is_doc_only = false,
 })
 
-function HighlightScreensaver:init()
-	self.ui.menu:registerToMainMenu(self)
-end
-
-function HighlightScreensaver:addToMainMenu(menu_items)
-	menu_items.highlight_screensaver = {
-		text = _("Highlight Screensaver"),
-		-- in which menu this should be appended
-		sorting_hint = "tools",
-		sub_item_table = {
-			{
-				text = _("Scan all book highlights"),
-				-- a callback when tapping
-				callback = function()
-					self:scanHighlights()
-				end,
-			},
-			{
-				text = _("Add to scannable directories"),
-				callback = function()
-					self:addToScannableDirectories()
-				end,
-			},
-		},
-	}
-end
-
-function HighlightScreensaver:scanHighlights()
-	local sidecars = utils.getAllSidecarPaths()
-	local total_saved = 0
-	for _, sidecar in ipairs(sidecars) do
-		local clippings = clipper.extractClippingsFromSidecar(sidecar)
-		for _, clipping in ipairs(clippings) do
-			clipper.saveClipping(clipping)
-			total_saved = total_saved + 1
-		end
-	end
-
-	local popup = InfoMessage:new({
-		text = _("Saved " .. total_saved .. "highlights"),
-	})
-	UIManager:show(popup)
-end
-
-function HighlightScreensaver:addToScannableDirectories()
-	local scannable_dirs = utils.getScannableDirs()
-	local curr_dir = self.ui.file_chooser.path
-	table.insert(scannable_dirs, curr_dir)
-
-	local unique_dirs = {}
-	local seen = {}
-	for _, dir in ipairs(scannable_dirs) do
-		if not seen[dir] then
-			table.insert(unique_dirs, dir)
-			seen[dir] = true
-		end
-	end
-
-	local dir = utils.getPluginDir()
-	local attr = lfs.attributes(dir)
-	if not attr then
-		local ok, err = utils.makeDir(utils.getPluginDir())
-		if not ok then
-			error(tostring(err))
-		end
-	end
-
-	local file, err = io.open(utils.getScannableDirsFilePath(), "w")
-	if not file then
-		error("Failed to open scannable-dirs file: " .. tostring(err))
-	end
-	file:write(json.encode(unique_dirs))
-	file:close()
-
-	local popup = InfoMessage:new({
-		text = _("Added to scannable directories: " .. curr_dir),
-	})
-	UIManager:show(popup)
-end
+function HighlightScreensaver:init() end
 
 return HighlightScreensaver
