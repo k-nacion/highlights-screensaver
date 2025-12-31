a832310e6ea0a7b94439b54042a70bf36ecd733b
k-nacion <nacionkenn@gmail.com>
2025-12-31 23:50:17 +0800

    Removed dead code and refactor _menu_builders.lua to _menu_injector.lua as it's job is to inject the menus to the main.

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
-local menu_builders = require("menu_builders._menu_builders")
+local menu_injector = require("menu_builders._menu_injector")
 local screensaver_patch = require("screensaver_patch")
 
 -- Patch Screensaver.show
 screensaver_patch.patchScreensaverShow()
 
 -- Patch dofile to inject menu
-menu_builders.patchDofileMenus()
+menu_injector.patchDofileMenus()
 
 local HighlightsScreensaver = WidgetContainer:extend({
     name = "Highlights Screensaver",
